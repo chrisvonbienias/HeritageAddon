e1aea2a3f93cd1160e2c34aab50f3af827ef27ba
Krzysztof Bieniek <chrisvonbienias@gmail.com>
2020-12-10 20:36:40 +0100

    Vertex Color works

--- vertex_color.py
+++ vertex_color.py
@@ -1,7 +1,6 @@
 import bpy
-import random
-import math
-import bmesh
+from collections import defaultdict
+from mathutils import Vector
 
 class VIEW3D_OT_vertexColor(bpy.types.Operator):
 
@@ -10,28 +9,31 @@ class VIEW3D_OT_vertexColor(bpy.types.Operator):
     bl_description = "Selects vertices based on color ID"
 
     def execute(self, context):
+
+        obj = bpy.context.active_object
+        col = obj.data.vertex_colors.active
+        polygons = obj.data.polygons
+
+        color = Vector((1, 1, 0, 0))
+        epsilon = 0.01
+
+        vertex_map = defaultdict(list)
+
+        for poly in polygons:
+            for v_ix, l_ix in zip(poly.vertices, poly.loop_indices):
+                vertex_map[v_ix].append(l_ix)
         
+        for v_ix, l_ixs in vertex_map.items():
+            for l_ix in l_ixs:
+                
+                comp = (color - Vector(col.data[l_ix].color)).length_squared
+
+                if comp < 1.0 + epsilon:
+                    
+                    obj.data.vertices[v_ix].select = True
+                    
         bpy.ops.object.editmode_toggle()
-        so = bpy.context.active_object
-        #vertCol = bpy.types.Mesh.vertex_colors
-        vertCol = so.data.
-        bm = bmesh.from_edit_mesh(so.data)
-
-        vertices = [e for e in bm.verts]
-
-        for vert in vertices:
-            
-            rand = round(random.random())
-            
-            if rand :
-
-                vert.select = True
-
-            else:
-
-                vert.select = False
-
-        bmesh.update_edit_mesh(so.data, True)
-        bm.free()
+        bpy.ops.mesh.select_mode(type='VERT')
+        bpy.ops.mesh.select_mode(type='FACE')    
 
         return {"FINISHED"}
